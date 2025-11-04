db964f316a3a38cb13edbd9c8c553c82427791c5
Eduardo Henrique <eduuh11@outlook.com>
2025-11-03 21:40:54 -0300

    Limpeza da tabela

--- Explicação.md
+++ Explicação.md
@@ -5,3 +5,8 @@ fiz sua ativação
 Localizei um dataset com informações bagunçadas e iniciei sua limpeza
 
 #correção
+exporto a tabela cafe
+
+renomeio as colunas
+
+substituo os valores nulos e erros por valores reais
--- Limpeza.py
+++ Limpeza.py
@@ -8,4 +8,14 @@ cafe = pd.read_csv("./Tabela/cafe.csv")
 cafe.to_excel("./Tabela/cafe.xlsx", index=False)
 
 # %%
-cafe 
+
+renamed_columns = {
+                            "Transaction ID":"Id de Transação", "Quantity": "Quantidade", "Price Per Unit": "Preço por Unidade", "Total Spend": "Total Gasto", "Payment Method": "Forma de Pagamento", "Location": "Local", "Transaction Date": "Data de Transação"}
+cafe.rename (columns=renamed_columns, inplace= True)
+cafe
+
+# %%
+
+replace = {"UNKNOWN": "Informação não encontrada", "ERROR": "Incorreto"}
+cafe = cafe.replace(replace)
+cafe.fillna({"Forma de Pagamento" : "Item não encontrado", "Item" : "Pagamento não registrado" })
